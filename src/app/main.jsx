1b1d806df42eca6d378e6964a38e6058d5587bdf
Kirill Komlev <kirillkomlev02@gmail.com>
2025-07-28 21:22:38 +0300

    Создал первоначальную структуру

---
+++ src/app/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
+
+const root = document.getElementById('root')
+
+ReactDOM.createRoot(root).render(
+	<BrowserRouter>
+		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit tempora vitae ullam commodi quia consectetur recusandae hic, maxime exercitationem eos fuga quis deserunt!
+		Consectetur ipsam, maiores amet modi tempora cupiditate.
+	</BrowserRouter>
+)
